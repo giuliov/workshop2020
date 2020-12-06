444fd7387fd4684d586e3716bf20ab3af7d6b161
Giulio Vian <giuliovdev@hotmail.com>
2020-12-06 22:08:28 +0100

    New behaviour controlled by Feature Flag

--- WorkshopAPI/src/API/CalcFunction.cs
+++ WorkshopAPI/src/API/CalcFunction.cs
@@ -21,9 +21,27 @@ namespace WorkshopAPI
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             int result;
+            if (IsFeatureOn("FF_USE_V2_ALGORITHM"))
+            {
+                log.LogInformation("V2 Algorithm.");
+                result = (2 * anInteger + 42) / 3;
+            }
+            else
+            {
                 log.LogInformation("V1 Algorithm.");
                 result = (anInteger + 42) / 2;
+            }
             return new OkObjectResult(result);
+        }
+
+        static readonly string[] onValues = new string[] { "true", "1", "yes", "on" };
+        static bool IsFeatureOn(string featureFlagName)
+        {
+            string featureFlag = Environment.GetEnvironmentVariable(featureFlagName, EnvironmentVariableTarget.Process);
+            if (featureFlag == null)
+                return false;
+            featureFlag = featureFlag.ToLower();
+            return onValues.Any(s => s == featureFlag);
         }
     }
 }
--- WorkshopAPI/src/tests/WorkshopAPI.UnitTests/CalcFunctionTests.cs
+++ WorkshopAPI/src/tests/WorkshopAPI.UnitTests/CalcFunctionTests.cs
@@ -17,6 +17,21 @@ namespace WorkshopAPI.UnitTests
         {
             // Arrange
             var request = TestFactory.CreateHttpRequest();
+            Environment.SetEnvironmentVariable("FF_USE_V2_ALGORITHM", null, EnvironmentVariableTarget.Process);
+            // Act
+            var response = (OkObjectResult)await CalcFunction.Run(request, inputValue, logger);
+            // Assert
+            Assert.Equal(expectedResult, response.Value);
+        }
+
+        [Theory]
+        [InlineData(0, 14)]
+        [InlineData(6, 18)]
+        public async void Version2_algorithm(int inputValue, int expectedResult)
+        {
+            // Arrange
+            var request = TestFactory.CreateHttpRequest();
+            Environment.SetEnvironmentVariable("FF_USE_V2_ALGORITHM", "true", EnvironmentVariableTarget.Process);
             // Act
             var response = (OkObjectResult)await CalcFunction.Run(request, inputValue, logger);
             // Assert
